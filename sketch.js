6dc6cce040f46eef9841dc45f5bc852e858f91ae
Raghav-17 <95995682+Raghav-17@users.noreply.github.com>
2022-01-16 17:30:36 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+
+  for (let index = 0; index < marks.length; index++) {
+   
+  if (marks [index] >=45) {
+    
+    console.log (marks[index])
+  }  
+    
+  }
+
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
